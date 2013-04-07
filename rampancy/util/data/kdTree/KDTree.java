1e07950c57d5e86cbc91a37ac3f5ae53a4e2934a
Matt Chun-Lum <mchunlum@gmail.com>
2013-04-07 13:32:05 -0700

    Finish KD tree implementation and tests

--- rampancy/util/data/kdTree/KDTree.java
+++ rampancy/util/data/kdTree/KDTree.java
@@ -1,6 +1,7 @@
 package rampancy.util.data.kdTree;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class KDTree<T> {
 	protected int maxBucketSize;
@@ -8,22 +9,36 @@ public class KDTree<T> {
 	protected KDNode<T> root;
 
 	public KDTree(int maxBucketSize) {
+		this.maxBucketSize = maxBucketSize;
 		this.dataPoints = new ArrayList<KDPoint<T>>();
 		this.root = null;
 	}
 	
+	public KDTree(int maxBucketSize, List<KDPoint<T>> seed) {
+		this.maxBucketSize = maxBucketSize;
+		this.dataPoints = new ArrayList<KDPoint<T>>(seed);
+		this.rebalance();
+	}
+	
 	public void add(KDPoint<T> value) {
-		dataPoints.add(value);
+		this.dataPoints.add(value);
 		if (this.root == null) {
 			this.root = new KDNode<T>(this.maxBucketSize);
 		}
 		this.root.add(value);
 	}
 
-	// TODO: maybe determine if I have to cap the list
 	public void rebalance() {
 		this.root = null;
-		this.root = new KDNode<T>(this.maxBucketSize, dataPoints);
+		this.root = new KDNode<T>(this.maxBucketSize, this.dataPoints);
+	}
+	
+	public void rebalance(int pointCap) {
+		if (this.dataPoints.size() > pointCap) {
+			int diff = this.dataPoints.size() - pointCap;
+			this.dataPoints = new ArrayList<KDPoint<T>>(this.dataPoints.subList(diff, this.dataPoints.size()));
+		}
+		rebalance();
 	}
 
 	public KDPoint<T> nearestNeighbor(KDPoint<T> query) {
@@ -32,9 +47,13 @@ public class KDTree<T> {
 		return search.best;
 	}
 	
-	// find nearest neighbor leaf
-	// unwind recursion
+	public ArrayList<KDPoint<T>> kNearestNeighbors(KDPoint<T> query, int k) {
+		KNNSearch<T> search = new KNNSearch<T>(query, k);
+		recursiveKNearestNeighbor(this.root, search);
+		return search.getResult();
+	}
 	
+	// separate these for speed
 	protected void recursiveNearestNeighbor(KDNode<T> root, NNSearch<T> search) {
 		if (root == null) {
 			return;
@@ -75,6 +94,44 @@ public class KDTree<T> {
 			recursiveNearestNeighbor(opposite, search);
 		}
 	}
+
+	protected void recursiveKNearestNeighbor(KDNode<T> root, KNNSearch<T> search) {
+		if (root == null) {
+			return;
+		}
+	
+		int featureIndex = root.featureIndex;
+	
+		double distance = 0;
+		
+		if (featureIndex < 0) {
+			for (KDPoint<T> point : root.bucket) {
+				distance = point.distanceTo(search.query);
+				if (distance < search.worstDistance || search.candidates.size() < search.numNeighbors) {
+					search.addCandidate(point, distance);
+				}
+			}
+			return;
+		}
+		
+		distance = root.value.distanceTo(search.query);
+		if (distance < search.worstDistance || search.candidates.size() < search.numNeighbors) {
+			search.addCandidate(root.value, distance);
+		}
+		
+		KDNode<T> opposite = null;
+		if (search.query.features[featureIndex] < root.value.features[featureIndex]) {
+			recursiveKNearestNeighbor(root.left, search);
+			opposite = root.right;
+		} else {
+			recursiveKNearestNeighbor(root.right, search);
+			opposite = root.left;
+		}
+		
+		if (Math.abs(search.query.features[featureIndex] - root.value.features[featureIndex]) < search.worstDistance) {
+			recursiveKNearestNeighbor(opposite, search);
+		}
+	}
 }	
 
 class NNSearch<T> {
@@ -91,18 +148,46 @@ class NNSearch<T> {
 
 class KNNSearch<T> {
 	int numNeighbors;
-	KDPoint<T> target;
-	ArrayList<KDNode<T>> path;
+	KDPoint<T> query;
 	ArrayList<KDPoint<T>> candidates;
-	double currentBest;
-	double currentWorst;
+	ArrayList<Double> distances;
+	double bestDistance;
+	double worstDistance;
 	
-	public KNNSearch(int numNeighbors, KDPoint<T> target) {
+	public KNNSearch(KDPoint<T> query, int numNeighbors) {
 		this.numNeighbors = numNeighbors;
-		this.target = target;
-		this.path = new ArrayList<KDNode<T>>();
+		this.query = query;
 		this.candidates = new ArrayList<KDPoint<T>>();
-		this.currentBest = -1;
-		this.currentWorst = -1;
+		this.distances = new ArrayList<Double>();
+		this.bestDistance = Double.POSITIVE_INFINITY;
+		this.worstDistance = Double.POSITIVE_INFINITY;
+	}
+	
+	public void addCandidate(KDPoint<T> point, double distance) {
+		if (distance < this.bestDistance) {
+			this.bestDistance = distance;
+		}
+		int size = candidates.size();
+		int i = 0;
+		for (; i < size; i++) {
+			if (distance < this.distances.get(i)) {
+				break;
+			}
+		}
+		
+		this.candidates.add(i, point);
+		this.distances.add(i, distance);
+	
+		int worstIndex = Math.min(candidates.size(), numNeighbors) - 1;
+		if (worstIndex >= 0) {
+			this.worstDistance = distances.get(worstIndex);
+		}
+	}
+	
+	public ArrayList<KDPoint<T>> getResult() {
+		if (this.candidates.size() > this.numNeighbors) {
+			return new ArrayList<KDPoint<T>>(this.candidates.subList(0, this.numNeighbors));
+		}
+		return this.candidates;
 	}
 }
--- rampancy/util/data/kdTree/KDTreeTest.java
+++ rampancy/util/data/kdTree/KDTreeTest.java
@@ -1,39 +1,82 @@
 package rampancy.util.data.kdTree;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class KDTreeTest {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testKDTree() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testAdd() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public void testRebalance() {
-		fail("Not yet implemented"); // TODO
+	
+	KDTree<String> tree;
+	
+	protected void buildDefaultTree() {
+		ArrayList<KDPoint<String>> seed = new ArrayList<KDPoint<String>>();
+		
+		seed.add(new KDPoint<String>("a", new double[]{12, 16}));
+		seed.add(new KDPoint<String>("b", new double[]{15,  8}));
+		seed.add(new KDPoint<String>("c", new double[]{ 5, 18}));
+		seed.add(new KDPoint<String>("d", new double[]{18,  5}));
+		seed.add(new KDPoint<String>("e", new double[]{16, 15}));
+		seed.add(new KDPoint<String>("f", new double[]{ 2,  5}));
+		seed.add(new KDPoint<String>("g", new double[]{ 7, 10}));
+		seed.add(new KDPoint<String>("h", new double[]{ 8,  7}));
+		seed.add(new KDPoint<String>("i", new double[]{ 5,  5}));
+		seed.add(new KDPoint<String>("j", new double[]{19, 12}));
+		seed.add(new KDPoint<String>("k", new double[]{10,  2}));
+		
+		this.tree = new KDTree<String>(3, seed);
 	}
 
 	@Test
 	public void testNearestNeighbor() {
-		fail("Not yet implemented"); // TODO
+		buildDefaultTree();
+		KDPoint<String> query = new KDPoint<String>("x", new double[]{13, 2});
+		KDPoint<String> nearest = this.tree.nearestNeighbor(query);
+		assertEquals("k", nearest.value);
+		
+		query = new KDPoint<String>("y", new double[]{10, 8});
+		nearest = this.tree.nearestNeighbor(query);
+		assertEquals("h", nearest.value);
 	}
-
+	
+	@Test
+	public void testKNearestNeighbors() {
+		buildDefaultTree();
+		KDPoint<String> query = new KDPoint<String>("x", new double[]{12, 9});
+		ArrayList<KDPoint<String>> nearest = this.tree.kNearestNeighbors(query, 2);
+		assertEquals(2, nearest.size());
+		
+		ArrayList<String> expected = new ArrayList<String>();
+		expected.add("h");
+		expected.add("b");
+		
+		assertTrue(expected.contains(nearest.get(0).value));
+		assertTrue(expected.contains(nearest.get(1).value));
+		
+		nearest = this.tree.kNearestNeighbors(query, 3);
+		assertEquals(3, nearest.size());
+		
+		expected.add("g");
+		assertTrue(expected.contains(nearest.get(0).value));
+		assertTrue(expected.contains(nearest.get(1).value));
+		assertTrue(expected.contains(nearest.get(2).value));
+		
+		nearest = this.tree.kNearestNeighbors(query, 11);
+		assertEquals(11, nearest.size());
+	}
+	
+	@Test
+	public void testRebalanceWithPruning() {
+		buildDefaultTree();
+		assertEquals(11, this.tree.dataPoints.size());
+		assertEquals("a", this.tree.dataPoints.get(0).value);
+		assertEquals("b", this.tree.dataPoints.get(1).value);
+		
+		this.tree.rebalance(9);
+		assertEquals(9, this.tree.dataPoints.size());
+		assertEquals("c", this.tree.dataPoints.get(0).value);
+		assertEquals("d", this.tree.dataPoints.get(1).value);
+	}
 }
